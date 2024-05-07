70efcb2cdd4bd89f556173e026bcc4e3c6f584aa
OmarAsharf62 <omarbondok62@gmail.com>
2024-05-07 18:16:17 +0300

    setup the application and added the svg package

---
+++ lib/features/presentation/views/my_cart_view.dart
@@ -0,0 +1,13 @@
+import 'package:easy_pay/features/presentation/views/widgets/my_card_view_body.dart';
+import 'package:flutter/material.dart';
+
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: MyCartViewBody(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_pay/features/presentation/views/my_cart_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class EasyPay extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text("Hello, World"),
+      home: MyCartView(),
     );
   }
 }
